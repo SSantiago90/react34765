97c44c60abc92525455c81240871f60b5574459d
ssantiago90 <salkin.santiago@gmail.com>
2022-09-19 17:16:37 -0300

    img y cards updated

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -11,14 +11,8 @@ function ItemListContainer(props) {
         <Card
           img="https://www.seoptimer.com/es/blog/wp-content/uploads/2020/06/uiux.png"
           title="UX/UI"
-          detail="Lorem Ipsum"
-          price="85"
-        />
-        <Card
-          img="https://blog.desafiolatam.com/wp-content/uploads/2019/04/react-galaxia.png"
-          title="React"
-          detail="Lorem Ipsum"
-          price="75"
+          detail="Aprende Diseño UX/UI desde cero!"
+          price="65"
         />
         <Card
           img="https://soyhorizonte.com/wp-content/uploads/2020/10/Javascript-by-SoyHorizonte.jpg"
@@ -27,22 +21,28 @@ function ItemListContainer(props) {
           price="59"
         />
         <Card
-          img="https://www.seoptimer.com/es/blog/wp-content/uploads/2020/06/uiux.png"
-          title="UX/UI"
-          detail="Lorem Ipsum"
-          price="85"
-        />
-        <Card
           img="https://blog.desafiolatam.com/wp-content/uploads/2019/04/react-galaxia.png"
           title="React"
-          detail="Lorem Ipsum"
+          detail="Aprende React desde cero!"
           price="75"
         />
         <Card
-          img="https://soyhorizonte.com/wp-content/uploads/2020/10/Javascript-by-SoyHorizonte.jpg"
-          title="Javascript"
+          img="https://www.atsistemas.com/dam/jcr:20b575da-1767-43dc-b18f-4f2a9f722877/1375-775-tailwind-css.png"
+          title="Tailwind"
           detail="Aprende Javascript desde cero!"
-          price="59"
+          price="35"
+        />
+        <Card
+          img="https://noviello.it/content/images/2020/05/nextjs.jpg"
+          title="NextJS"
+          detail="Aprende Javascript desde cero!"
+          price="40"
+        />
+        <Card
+          img="https://miro.medium.com/max/850/0*BRrBBgjCGAzRniV8.jpg"
+          title="VueJS"
+          detail="Aprende VueJS desde cero!"
+          price="70"
         />
       </FlexWrapper>
     </div>
