1e3c59c552f995b1e7b2a50b6ec745200631a2c5
ssantiago90 <salkin.santiago@gmail.com>
2022-10-22 15:52:10 -0300

    fix swal

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { createBuyOrder } from "../../services/firebase";
 import { useNavigate } from "react-router-dom";
 
-import "sweetalert2/src/sweetalert2.scss";
+/* import "sweetalert2/src/sweetalert2.scss"; */
 import Swal from "sweetalert2/dist/sweetalert2.js";
 
 import InputForm from "./InputForm";
@@ -26,7 +26,7 @@ export default function UserForm({ cart, getTotalPrice }) {
   }
 
   function onSubmit(evt) {
-    evt.preventDefault();   
+    evt.preventDefault();
 
     const orderData = {
       buyerData: userData,
